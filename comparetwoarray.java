adae5ba592c4d224caefa89ff1bbd09794540413
Mehar2006 <meharmehar@6266gamil.com>
2025-04-17 13:40:36 +0530

    comparetwoarray

---
+++ comparetwoarray.java
@@ -0,0 +1,16 @@
+public class comparetwoarray {
+    public static void main(String[] args) {
+        int [] arr1={1,2,3,4,5};
+        int [] arr2={1,2,3,4,5};
+         for(int i=0;i<5;i++)
+        if(arr1[i] == arr2[i])
+        {
+         System.out.println("the array is equal");
+
+        }
+        else{
+            System.out.println("the array is not equal");
+        
+    }
+}
+}
