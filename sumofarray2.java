5c0c7a3cf67cd55c04256f50bafe975a6a739e15
Mehar2006 <meharmehar@6266gamil.com>
2025-04-17 13:18:08 +0530

    sumofarray2

--- sumofarray2.java
+++ sumofarray2.java
@@ -2,15 +2,15 @@ public class sumofarray2 {
     public static void main(String[] args) {
         int[] arr1={1,2,3,4,5};
         int[] arr2={1,2,3,4,5};
-        int sum= new int[ arr1.length];
-        sum=0;
+        int[] sum= new int[ arr1.length];
 
-        for( int i=0; i=arr1.length; i++)
+        for( int i=0; i<arr1.length; i++)
         {
             sum[i]=arr1[i]+arr2[i];
         }
+        System.out.println("the addition of array");
          for(int k=0; k<5;k++){
-             System.out.println(sum[i]);
+             System.out.println(sum[k]);
          }
             
         
