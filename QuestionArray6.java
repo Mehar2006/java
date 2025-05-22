59769b91d396a8368fc3e66c3e3277175b590d14
Mehar2006 <meharmehar@6266gamil.com>
2025-05-22 10:54:35 +0530

    errorhandling.java

--- QuestionArray6.java
+++ QuestionArray6.java
@@ -7,7 +7,7 @@ public class QuestionArray6 {
  {
     
         int[] arr = {1,2,3,4,5};
-        for(int i=arr.length-1;i>0;i--){
+        for(int i=arr.length-1;i>0n;i--){
             arr[i]=arr[i-1];
         }
         arr[0]=arr.length;
---
+++ errorhandling.java
@@ -0,0 +1,38 @@
+// public class errorhandling {
+//     public static void main(String[] args) {
+//          int a=10;int b=0;
+//          try{
+//             int c=a/b;
+//             System.out.println(c);
+//          }
+//         catch( Exception e){
+//             System.out.println(" we can not divided by zero");
+//         }
+    
+//         System.out.println("hello");
+//         System.out.println("hii");
+//         System.out.println("byee");
+// }
+//     }
+
+public class errorhandling {
+      public static void main(String[] args){
+        int [] arr={1,2,3,4};
+        try {
+            System.out.println(arr[6]);
+            
+        } catch ( ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+            System.out.println("we can not accec the index 6th because it is not in  bound   --"+e.getMessage());
+            
+
+        }
+        catch(ArithmeticException e){
+            System.out.println(e);
+        }
+       
+
+      }
+    }
+   // we can make mutliple catch with repect to try
+
