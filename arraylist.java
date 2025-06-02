5be019c73e18a65c4ead03a01dbbca6bda7118db
Mehar2006 <meharmehar@6266gamil.com>
2025-06-02 10:44:57 +0530

    arraylist.java

---
+++ arraylist.java
@@ -0,0 +1,45 @@
+
+
+// public class arraylist {
+//     public static void main(String[] args) {
+//         ArrayList<Integer>list= new ArrayList<>();
+//         list.add(1);
+//         list.add(0);
+//         list.add(4);
+//         Collections.sort(list);
+//         System.out.println(list);
+//     }
+    
+// }
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+class Student implements Comparable <Student >{
+    int age;
+    String name;
+
+    public Student(int age ,String name){
+        this.age=age;
+        this.name=name;
+    }
+    public String toString(){
+        return age + " " + name;
+    }
+
+    public int compareTo(Student s){
+         return this.age-s.age;
+    }
+}
+
+public class arraylist{
+    public static void main(String[] args) {
+        ArrayList<Student> list = new ArrayList<>();
+        list.add(new Student(12,"rahul"));
+        list.add(new Student(14,"rahul"));
+        Collections.sort(list);
+        for(Student s : list){
+            System.out.println(s.toString());
+        }
+    }}
