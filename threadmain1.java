fcc76e3aa526e1fa1ef0feabd221ac294038863d
Mehar2006 <meharmehar@6266gamil.com>
2025-06-04 11:17:07 +0530

    threadmain1.java

---
+++ threadmain1.java
@@ -0,0 +1,32 @@
+class Main extends Thread{
+    static int balance = 1000;
+    int withdrawal;
+    public Main(int withdrawal){
+        this.withdrawal = withdrawal;
+    }
+    public void run(){
+        synchronized(Main.class){
+            if(balance >withdrawal){
+            System.out.println("is going to withdrawal"+" "+withdrawal);
+            try {
+            Thread.sleep(2000);
+        }
+        catch(Exception e){
+        }
+        balance = balance - withdrawal;
+        System.out.println("remaining balance is "+balance);
+        }
+        else {
+            System.out.println("not enough balance for withdrawal");
+        }
+    }
+        }
+}
+public class threadmain1{
+    public static void main(String[] args) {
+        Main t1 = new Main(800);
+        t1.start();
+        Main t2 = new Main(500);
+        t2.start();
+    }
+}
