49287b67cda05b3390b0ebfb023ea63947dc5e41
Mehar2006 <meharmehar@6266gamil.com>
2025-06-04 10:05:07 +0530

    exampleofmultithreading.java

---
+++ exampleofmultithreading.java
@@ -0,0 +1,16 @@
+class MyRunnable implements  java.lang.Runnable{
+    public void run(){
+        System.out.println("thread using runnable interface");
+    }
+}
+
+public class exampleofmultithreading { 
+    public static void main(String[] args) {
+        MyRunnable obj = new MyRunnable();
+        Thread t1 = new Thread(obj);
+        t1.start();
+        System.out.println("main thread is running");
+        
+    }
+    
+}
